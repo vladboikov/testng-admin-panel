23b3a792222ae45fcb43a7058d1c5986f855a749
unknown <emptyspaces16il@gmail.com>
2024-06-11 23:37:40 -0500

    doLogin method refactored

--- src/main/java/pages/actions/SignInPage.java
+++ src/main/java/pages/actions/SignInPage.java
@@ -8,33 +8,41 @@ import pages.locators.SignInPageLocators;
 
 public class SignInPage extends BasePage {
 
-    public static SignInPageLocators signInLocators;
+    static SignInPageLocators signInLocators;
 
     public SignInPage(WebDriver driver) {
         super(driver);
-        signInLocators = new SignInPageLocators();
     }
 
     public static void doLogin(WebDriver driver, UserRole role) {
         new SignInPage(driver);
-        if(role.equals(UserRole.ADMIN)) {
-            signInLocators.emailInput.sendKeys(Credentials.AdminEmail);
-            signInLocators.passwordInput.sendKeys(Credentials.AdminPassword);
-        } else if (role.equals(UserRole.PAYIN_OPERATOR)) {
-            signInLocators.emailInput.sendKeys(Credentials.PayinOperatorEmail);
-            signInLocators.passwordInput.sendKeys(Credentials.PayinOperatorPassword);
-        } else if (role.equals(UserRole.PAYOUT_OPERATOR)) {
-            signInLocators.emailInput.sendKeys(Credentials.PayoutOperatorEmail);
-            signInLocators.passwordInput.sendKeys(Credentials.PayoutOperatorPassword);
-        } else if (role.equals(UserRole.OFFICE_ADMIN)) {
-            signInLocators.emailInput.sendKeys(Credentials.OfficeAdminEmail);
-            signInLocators.passwordInput.sendKeys(Credentials.OfficeAdminPassword);
-        } else if (role.equals(UserRole.MERCHANT)) {
-            signInLocators.emailInput.sendKeys(Credentials.MerchantEmail);
-            signInLocators.passwordInput.sendKeys(Credentials.MerchantPassword);
-        } else if (role.equals(UserRole.SUPPORT)) {
-            signInLocators.emailInput.sendKeys(Credentials.SupportEmail);
-            signInLocators.passwordInput.sendKeys(Credentials.SupportPassword);
+        signInLocators = new SignInPageLocators();
+
+        switch (role) {
+            case ADMIN:
+                signInLocators.emailInput.sendKeys(Credentials.AdminEmail);
+                signInLocators.passwordInput.sendKeys(Credentials.AdminPassword);
+                break;
+            case PAYIN_OPERATOR:
+                signInLocators.emailInput.sendKeys(Credentials.PayinOperatorEmail);
+                signInLocators.passwordInput.sendKeys(Credentials.PayinOperatorPassword);
+                break;
+            case PAYOUT_OPERATOR:
+                signInLocators.emailInput.sendKeys(Credentials.PayoutOperatorEmail);
+                signInLocators.passwordInput.sendKeys(Credentials.PayoutOperatorPassword);
+                break;
+            case OFFICE_ADMIN:
+                signInLocators.emailInput.sendKeys(Credentials.OfficeAdminEmail);
+                signInLocators.passwordInput.sendKeys(Credentials.OfficeAdminPassword);
+                break;
+            case MERCHANT:
+                signInLocators.emailInput.sendKeys(Credentials.MerchantEmail);
+                signInLocators.passwordInput.sendKeys(Credentials.MerchantPassword);
+                break;
+            case SUPPORT:
+                signInLocators.emailInput.sendKeys(Credentials.SupportEmail);
+                signInLocators.passwordInput.sendKeys(Credentials.SupportPassword);
+                break;
         }
         signInLocators.signInButton.click();
     }
